e4981e8fa22daa46ee414b871700ab9576d8ec82
Andesson <andessonlins93@gmail.com>
2024-12-16 19:23:53 -0300

    feat: add custom Log

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Andesson/APIGO/router"
+import (
+	"github.com/Andesson/APIGO/config"
+	"github.com/Andesson/APIGO/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
