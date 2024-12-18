523996a74d2d232c575b0d987bd7c3d7cf9d38d3
Andesson <andessonlins93@gmail.com>
2024-12-18 17:53:12 -0300

    feat: add Crud hedler logic

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -3,11 +3,15 @@ package handler
 import (
 	"net/http"
 
+	"github.com/Andesson/APIGO/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func ShowAllOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "LIST Opening",
-	})
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSucess(ctx, "list-opening", openings)
 }
