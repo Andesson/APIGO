b79b942cbfda8adc7b18681776abcef148bb47e8
Andesson <andessonlins93@gmail.com>
2024-12-18 16:00:49 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/Andesson/APIGO/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetPostgree()
+}
